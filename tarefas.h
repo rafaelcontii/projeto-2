f344b6c814d392a7160971e32e6c5727ebebe529
CassioSousa09 <148918527+CassioSousa09@users.noreply.github.com>
2024-04-12 23:09:09 -0300

    Update tarefas.h

--- tarefas.h
+++ tarefas.h
@@ -1,19 +1,44 @@
+
+#ifndef TAREFAS_H
+#define TAREFAS_H
+
+#define CHAR_CATEGORIA 100
+#define CHAR_DESCRICAO 300
 #define TOTAL 100
 
+#include <stdio.h>
+
 typedef struct {
-    int prioridade;
-    char descricao[300];
-    char categoria[100];
+  int prioridade;
+  char descricao[CHAR_DESCRICAO];
+  char categoria[CHAR_CATEGORIA];
+  char estado[20];
 } Tarefa;
 
-typedef enum {OK, MAX_TAREFA, SEM_TAREFAS, NAO_ENCONTRADO, ABRIR, FECHAR, ESCREVER, LER} ERROS;
+typedef enum {
+  OK,
+  MAX_TAREFA,
+  SEM_TAREFAS,
+  NAO_ENCONTRADO,
+  ABRIR,
+  FECHAR,
+  ESCREVER,
+  LER,
+  PRIORIDADE_INVALIDA,
+  OPERACAO_INVALIDA
+} ERROS;
 
-typedef ERROS (*funcao)(Tarefa[], int*);
+typedef ERROS (*funcao)(Tarefa[], int *);
 
 ERROS criar(Tarefa tarefas[], int *pos);
 ERROS deletar(Tarefa tarefas[], int *pos);
 ERROS listar(Tarefa tarefas[], int *pos);
 ERROS salvar(Tarefa tarefas[], int *pos);
 ERROS carregar(Tarefa tarefas[], int *pos);
+ERROS exportar(Tarefa tarefas[], int *pos);
+ERROS binario(Tarefa tarefas[], int *pos, char *nomeArquivo,
+                       int operacao);
 
 void clearBuffer();
+
+#endif
